7beb08560395b8cd1a3cba929233d938dc89e2c8
AndyLC346 <149219743+AndyLC346@users.noreply.github.com>
2025-09-08 09:58:20 -0500

    Users CRUD

---
+++ src/main/java/pe/edu/upc/backendgrupo1/servicesinterfaces/IUserService.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.backendgrupo1.servicesinterfaces;
+
+import pe.edu.upc.backendgrupo1.entities.User;
+
+import java.util.List;
+
+public interface IUserService {
+    public List<User> list();
+    public void insert(User user);
+    public void delete(int id);
+    public void update(User user);
+    public User listId(int id);
+}
