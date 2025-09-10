0a7a9532062a73abbebccd1e831379d3e083aa20
Andy Jheremy Lazaro Curia <149219743+AndyLC346@users.noreply.github.com>
2025-09-09 21:52:28 -0500

    ejmplo de commit

--- src/main/java/pe/edu/upc/backendgrupo1/servicesinterfaces/IUserService.java
+++ src/main/java/pe/edu/upc/backendgrupo1/servicesinterfaces/IUserService.java
@@ -5,6 +5,7 @@ import pe.edu.upc.backendgrupo1.entities.User;
 import java.util.List;
 
 public interface IUserService {
+    //hola
     public List<User> list();
     public void insert(User user);
     public void delete(int id);
