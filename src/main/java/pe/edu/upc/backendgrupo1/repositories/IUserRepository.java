a93fd5c0760eb61ed1d55a7c1ea8112b30182677
AndyLC346 <149219743+AndyLC346@users.noreply.github.com>
2025-09-11 22:22:50 -0500

    validacion merge intento 2

--- src/main/java/pe/edu/upc/backendgrupo1/repositories/IUserRepository.java
+++ src/main/java/pe/edu/upc/backendgrupo1/repositories/IUserRepository.java
@@ -3,7 +3,7 @@ package pe.edu.upc.backendgrupo1.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.backendgrupo1.entities.User;
-//validar merge
+//validar merge xd
 @Repository
 public interface IUserRepository extends JpaRepository<User, Integer> {
 }
