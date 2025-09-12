ade3ef600873404a5b221d8f4d65f9e5cccd5b67
AndyLC346 <149219743+AndyLC346@users.noreply.github.com>
2025-09-11 22:19:55 -0500

    Update IUserService.java

--- src/main/java/pe/edu/upc/backendgrupo1/servicesinterfaces/IUserService.java
+++ src/main/java/pe/edu/upc/backendgrupo1/servicesinterfaces/IUserService.java
@@ -5,7 +5,7 @@ import pe.edu.upc.backendgrupo1.entities.User;
 import java.util.List;
 
 public interface IUserService {
-    //hola456
+    //validar merge
     public List<User> list();
     public void insert(User user);
     public void delete(int id);
