65508867573431402a22f57e13451b173e1c443d
GHJhonatan <166961380+GHJhonatan@users.noreply.github.com>
2025-09-16 15:28:30 -0500

    Crud Recomendacion

--- src/main/java/pe/edu/upc/backendgrupo1/entities/Recomendacion.java
+++ src/main/java/pe/edu/upc/backendgrupo1/entities/Recomendacion.java
@@ -1,4 +1,66 @@
 package pe.edu.upc.backendgrupo1.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recomendaciones")
 public class Recomendacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRecomendacion;
+
+    @Column(length = 40, nullable = false)
+    private String tituloRecomendacion;
+
+    @Column(length = 200)
+    private String descripcionRecomendacion;
+
+    @Column(length = 50)
+    private String categoriaRecomendacion;
+
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario")
+    private User user;
+
+    public Long getIdRecomendacion() {
+        return idRecomendacion;
+    }
+
+    public void setIdRecomendacion(Long idRecomendacion) {
+        this.idRecomendacion = idRecomendacion;
+    }
+
+    public String getTituloRecomendacion() {
+        return tituloRecomendacion;
+    }
+
+    public void setTituloRecomendacion(String tituloRecomendacion) {
+        this.tituloRecomendacion = tituloRecomendacion;
+    }
+
+    public String getDescripcionRecomendacion() {
+        return descripcionRecomendacion;
+    }
+
+    public void setDescripcionRecomendacion(String descripcionRecomendacion) {
+        this.descripcionRecomendacion = descripcionRecomendacion;
+    }
+
+    public String getCategoriaRecomendacion() {
+        return categoriaRecomendacion;
+    }
+
+    public void setCategoriaRecomendacion(String categoriaRecomendacion) {
+        this.categoriaRecomendacion = categoriaRecomendacion;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/pe/edu/upc/backendgrupo1/repositories/IRecomendacionRepository.java
+++ src/main/java/pe/edu/upc/backendgrupo1/repositories/IRecomendacionRepository.java
@@ -1,4 +1,11 @@
 package pe.edu.upc.backendgrupo1.repositories;
 
-public interface IRecomendacionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.backendgrupo1.entities.Recomendacion;
+
+@Repository
+public interface IRecomendacionRepository extends JpaRepository<Recomendacion, Long> {
+
+
 }
