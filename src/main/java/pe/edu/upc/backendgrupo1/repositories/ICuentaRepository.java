5ece87856c67e1d499e0297cfe064bb373ef060a
JohnnyM2005 <u202313751@upc.edu.pe>
2025-09-30 13:25:55 -0500

    Update ICuentaRepository.java
    Query cuentas activas de un usuario parte cuenta repository

--- src/main/java/pe/edu/upc/backendgrupo1/repositories/ICuentaRepository.java
+++ src/main/java/pe/edu/upc/backendgrupo1/repositories/ICuentaRepository.java
@@ -1,9 +1,24 @@
 package pe.edu.upc.backendgrupo1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.backendgrupo1.entities.Cuenta;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ICuentaRepository extends JpaRepository<Cuenta, Integer> {
+    @Query(value = "SELECT id_cuenta, servicio_cuenta, nombre_cuenta, estado_cuenta, fecharegistro_cuenta, id_user " +
+            "FROM cuentas " +
+            "WHERE estado_cuenta = 'ACTIVO' " +
+            "AND id_user = :idUser " +
+            "AND LOWER(nombre_cuenta) LIKE CONCAT('%', LOWER(:nombre), '%') " +
+            "AND fecharegistro_cuenta >= :fecha", nativeQuery = true)
+    List<Object[]> buscarCuentasFiltradas(@Param("idUser") int idUser,
+                                          @Param("nombre") String nombre,
+                                          @Param("fecha") LocalDate fecha);
+
 }
