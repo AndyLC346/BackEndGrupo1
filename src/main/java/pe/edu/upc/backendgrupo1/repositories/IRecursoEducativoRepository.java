a3e7506ad6bd9adcf0d3e3bfa99a125b591c0e60
AngelSoria1 <u202316237@upc.edu.pe>
2025-10-03 21:05:11 -0500

    Recurso Query 2 repository added

--- src/main/java/pe/edu/upc/backendgrupo1/repositories/IRecursoEducativoRepository.java
+++ src/main/java/pe/edu/upc/backendgrupo1/repositories/IRecursoEducativoRepository.java
@@ -11,10 +11,16 @@ import java.util.List;
 
 @Repository
 public interface IRecursoEducativoRepository extends JpaRepository<Recurso, Integer> {
-    //
+
     @Query(value = "SELECT * FROM RecursoEducativo \n" +
             "WHERE tipoRecurso = :tipo AND nivelRecurso = :nivel", nativeQuery = true)
     public List<Recurso> buscarRecursoxTipoynivel(@Param("tipo") String tipo, @Param("nivel") String nivel);
+
+    @Query(value = "SELECT u.username,Count(r.id_user) as cantidad_recursos FROM recursoseducativos r inner join users u\n" +
+            "   on r.id_user=u.id_user\n" +
+            "   Where username = :username\n" +
+            "   group by u.username",nativeQuery = true)
+    public List<Recurso> buscarRecursoxTipoynivel(@Param("username") String username);
 
 
 
