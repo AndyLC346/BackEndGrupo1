7834331ed1d80be939cb877f8ad94b7b8c8f7ded
AndyLC346 <149219743+AndyLC346@users.noreply.github.com>
2025-10-05 17:39:39 -0500

    Query 1 de Usuarios

--- src/main/java/pe/edu/upc/backendgrupo1/servicesinterfaces/IUserService.java
+++ src/main/java/pe/edu/upc/backendgrupo1/servicesinterfaces/IUserService.java
@@ -1,5 +1,6 @@
 package pe.edu.upc.backendgrupo1.servicesinterfaces;
 
+import pe.edu.upc.backendgrupo1.dtos.TotalArchivosXUsersDTO;
 import pe.edu.upc.backendgrupo1.entities.Users;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface IUserService {
     public void delete(long id);
     public void update(Users user);
     public Users listId(long id);
+    public List<Object[]> TotalArchivosXUsers();
 }
