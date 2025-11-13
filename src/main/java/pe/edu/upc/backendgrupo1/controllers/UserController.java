be7bc2909ba22f0a5ade6aa3261548bd2db03d9f
Andy Jheremy Lazaro Curia <149219743+AndyLC346@users.noreply.github.com>
2025-11-12 20:39:22 -0500

    chau

--- src/main/java/pe/edu/upc/backendgrupo1/controllers/UserController.java
+++ src/main/java/pe/edu/upc/backendgrupo1/controllers/UserController.java
@@ -3,7 +3,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.backendgrupo1.dtos.TotalArchivosXUsersDTO;
 import pe.edu.upc.backendgrupo1.dtos.UserDTO;
